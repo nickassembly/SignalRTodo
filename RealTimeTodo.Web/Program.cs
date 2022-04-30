676114a7ce09e0d22e7d55e05e502b9b6b26c0f8
nickguerra <nickg@maliciousgames.com>
2022-04-30 14:03:17 -0500

    Added client and server projects

---
+++ RealTimeTodo.Web/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSpaStaticFiles(configure => {
+    configure.RootPath = "wwwroot";
+});
+
+var app = builder.Build();
+
+app.UseSpaStaticFiles();
+app.UseSpa(config => 
+{
+  config.UseProxyToSpaDevelopmentServer("http://localhost:8080");
+});
+
+app.Run();
