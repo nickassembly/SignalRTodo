66f2185544bed85e73465b54540321ba13291fa4
nickguerra <nickg@maliciousgames.com>
2022-05-11 07:08:20 -0500

    Added subscribe and unsubscribe count and list methods

--- RealTimeTodo.Web/Hubs/ToDoHub.cs
+++ RealTimeTodo.Web/Hubs/ToDoHub.cs
@@ -18,4 +18,45 @@ public class ToDoHub : Hub
        await Clients.Caller.SendAsync("updatedToDoList", results);
     }
 
+    public async Task GetList(int listId)
+    {
+       var result = await todoRepository.GetList(listId);
+
+       await Clients.Caller.SendAsync("updatedListData", result); 
+    } 
+
+    public async Task SubscribeToCountUpdates()
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, "Counts");
+    }
+
+     public async Task UnsubscribeToCountUpdates()
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Counts");
+    }
+
+     public async Task SubscribeToListUpdates(int listId)
+    {
+        var groupName = $"list-updates-{listId}";
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+    }
+
+     public async Task UnsubscribeToListUpdates(int listId)
+    {
+         var groupName = $"list-updates-{listId}";
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+    }
+
+    public async Task AddToDoList(int listId, string text)
+    {
+        await todoRepository.AddToDoItem(listId, text);
+
+        // notify list count updates
+        // notify list viewers on updates
+        
+    }
+
+    // ToggleToDoItem 32:00
+
+
 }
